b8c0abb672c694370a3df5a2d52ccaf0d21707a8
Karthick Kumaravel <kaarthick.p.k@gmail.com>
2017-08-31 11:09:13 +0530

    python

---
+++ Disk_space_alert/disk_alert_b.py
@@ -0,0 +1,47 @@
+############################################################    Email the Disk Alert    ############################################################  
+
+#!/usr/bin/python
+
+import os
+import subprocess
+import smtplib
+
+subprocess.call("./disk_alert_a.sh")                           # Bash script to check the disk space (the script is in the same folder)                                   
+
+a=os.stat('/tmp/result.txt').st_size==0
+
+if ( a == False ):
+
+	TO = 'name@domain.com'                                         # To email-address
+	SUBJECT = '##### Disk Space Alert...!!!!#####'                 # Subject of the email
+	
+	f = open('/tmp/result.txt', 'r')                               # '/tmp/result.txt' is the file contains the output of the script-disk_alert_a.sh
+	TEXT = f.read()
+	f.close()
+	
+	
+	sender = 'name@domain.com'                                      # Sender email-address
+	passwd = 'Password'                                             # Sender email-address's password
+
+	server = smtplib.SMTP('star.sixthstar.in', 587)                 # SMTP of the server
+	server.ehlo()                                                   # ESMTP
+	server.starttls()                                               # TLS
+	server.ehlo()
+	server.login(sender, passwd)
+	
+	
+	BODY = '\r\n'.join([
+        	'To: %s' % TO,
+		'From: %s' % sender,
+		'subject: %s' % SUBJECT,
+		'',
+		TEXT
+		])
+	try:
+		server.sendmail(sender, [TO], BODY)
+		print 'email sent'
+  except:
+    print 'error'
+
+
+Note: This script should be used along with "disk_alert_a.sh" in the same folder to achive the result.
