9853525605ae7c0a37bfa9482458dcd4cdd65a72
Karthick Kumaravel <kaarthick.p.k@gmail.com>
2017-09-01 16:20:22 +0530

    python

--- Disk_space_alert/disk_alert_b.py
+++ Disk_space_alert/disk_alert_b.py
@@ -3,16 +3,16 @@
 #!/usr/bin/python
 
 import os
-import subprocess
 import smtplib
 
-subprocess.call("./disk_alert_a.sh")                        # Bash script to check the disk space (the script is in the same folder)                                   
+os.system("./disk_alert_a.sh")                        	   # Bash script to check the disk space (the script is in the same folder)                                   
 
 a=os.stat('/tmp/result.txt').st_size==0
 
 if ( a == False ):
 
 	TO = 'name@domain.com'                              # To email-address
+	BCC = 'name@domain.com'				    # Bcc email address
 	SUBJECT = '##### Disk Space Alert...!!!!#####'      # Subject of the email
 	
 	f = open('/tmp/result.txt', 'r')                    # '/tmp/result.txt' is the file contains the output of the script-disk_alert_a.sh
@@ -23,7 +23,7 @@ if ( a == False ):
 	sender = 'name@domain.com'                          # Sender email-address
 	passwd = 'Password'                                 # Sender email-address's password
 
-	server = smtplib.SMTP('smtp.domain.com', 587)     # SMTP of the server
+	server = smtplib.SMTP('smtp.domain.com', 587)       # SMTP of the server
 	server.ehlo()                                       # ESMTP
 	server.starttls()                                   # TLS
 	server.ehlo()
@@ -38,7 +38,7 @@ if ( a == False ):
 		TEXT
 		])
 	try:
-		server.sendmail(sender, [TO], BODY)
+		server.sendmail(sender, [TO, BCC], BODY)
 		print 'email sent'
         except:
                 print 'error'
