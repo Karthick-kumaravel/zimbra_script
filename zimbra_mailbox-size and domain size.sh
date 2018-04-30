f2ab24e89cadbfbb2ac4e9777d29e69f6c908cf6
Karthick Kumaravel <kaarthick.p.k@gmail.com>
2018-04-30 12:29:33 +0530

    Update zimbra_mailbox-size and domain size.sh

--- zimbra_mailbox-size and domain size.sh
+++ zimbra_mailbox-size and domain size.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-all_accounts=`zmprov -l gaa | grep "Domain name"` 
+> results.txt
+> final.txt
+> num.txt
+
+all_accounts=`zmprov -l gaa | grep "demo.sixthstar.in"` 
 for account in $all_accounts
 do
 mbox_size=`zmmailbox -z -m $account gms`
@@ -9,19 +13,19 @@ echo "Mailbox size of $account = $mbox_size"
 a=`cat results.txt | awk '{ print $7 }'`
 if [[ $a == B ]]; 
 	then
-	b=`cat results.txt | awk '{ print $6 }'`
-	`calc() { awk "BEGIN{print $*}"; } ; calc $b/1024/1024/1024 >> num.txt`
-elif [[ $a == KB ]]; 
+			b=`cat results.txt | awk '{ print $6 }'`
+			`calc() { awk "BEGIN{print $*}"; } ; calc $b/1024/1024/1024 >> num.txt`
+	elif [[ $a == KB ]]; 
 	then
-	c=`cat results.txt | awk '{ print $6 }'`
-	`calc() { awk "BEGIN{print $*}"; } ; calc $c/1024/1024 >> num.txt`
-elif [[ $a == MB ]]; 
+			c=`cat results.txt | awk '{ print $6 }'`
+            `calc() { awk "BEGIN{print $*}"; } ; calc $c/1024/1024 >> num.txt`
+	elif [[ $a == MB ]]; 
 	then
-	d=`cat results.txt | awk '{ print $6 }'`
-	`calc() { awk "BEGIN{print $*}"; } ; calc $d/1024 >> num.txt`
-elif [[ $a == GB ]];
+	    	d=`cat results.txt | awk '{ print $6 }'`
+	    	`calc() { awk "BEGIN{print $*}"; } ; calc $d/1024 >> num.txt`
+	elif [[ $a == GB ]];
 	then
-	`cat results.txt | awk '{ print $6 }' >> num.txt`
+	    	`cat results.txt | awk '{ print $6 }' >> num.txt`
 fi  
 done
 
@@ -30,10 +34,10 @@ for i in `cat num.txt`
 do
 	sum=`calc() { awk "BEGIN{print $*}"; } ; calc $sum+$i`
 done
-    e=`cat results.txt | awk '{ print $4 }' | awk -F@ '{ print $2 }'`
-    f=$(echo -e "\033[32mTotal size of the domain "$e" is $sum GB\033[0m")
-    echo ""
-    echo $f >> final.txt
-	echo $f    
-    echo ""
-    exit 0 
+
+e=`cat results.txt | awk '{ print $4 }' | awk -F@ '{ print $2 }'`
+f=$(echo -e "\033[32mTotal size of the domain "$e" is $sum GB\033[0m")
+echo ""
+echo $f
+echo ""
+exit 0 
